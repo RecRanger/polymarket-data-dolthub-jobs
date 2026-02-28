78b7e21598f79ab79eeca0e5e521b247f04801cd
RecRanger <168371178+RecRanger@users.noreply.github.com>
2026-02-28 16:15:22 -0700

    Update schema

--- dolt_schemas/silver_market_outcomes.sql
+++ dolt_schemas/silver_market_outcomes.sql
@@ -1,6 +1,8 @@
 
 CREATE TABLE silver_market_outcomes (
+    outcome_id VARCHAR(255) NOT NULL, 
     outcome_slug VARCHAR(355) NOT NULL, 
+    outcome_index SMALLINT NOT NULL, 
     market_id VARCHAR(255) NOT NULL, 
     market_slug VARCHAR(255) NOT NULL, 
     question VARCHAR(255) NOT NULL, 
@@ -9,6 +11,6 @@ CREATE TABLE silver_market_outcomes (
     clob_token_id VARCHAR(255) NOT NULL, 
     db_created_at DATETIME DEFAULT now() NOT NULL, 
     db_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL, 
-    PRIMARY KEY (outcome_slug)
+    PRIMARY KEY (outcome_id)
 )
 
